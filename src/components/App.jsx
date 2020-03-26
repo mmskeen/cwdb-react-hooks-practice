2ee5cfe41647b55c92f62802e9cc29f2806e2337
Michael Skeen <mmskeen@gmail.com>
2020-03-26 11:16:02 -0600

    updated time dynamically

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now);
+  function updateTime() {
+    setTime(new Date().toLocaleTimeString());
+  }
+  setInterval(updateTime, 2000);
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
 
 export default App;
+
+//Challenge:
+//1. Given that you can get the current time using:
+//Show the latest time in the <h1> when the Get Time button
+//is pressed.
+
+//2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
